7b0e198608b347ce2a29c312b72892ae34dd81fe
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-01 00:02:37 -0300

    first commit

---
+++ testeGit/src/main/java/Main/TesteGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author Leonardo Schmitt
+ */
+public class TesteGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
