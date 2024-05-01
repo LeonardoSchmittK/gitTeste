000fdd452cfbcd535d7e464bae3228eb0e9d6b64
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-01 00:34:44 -0300

    add 222

--- testeGit/src/main/java/Main/TesteGit.java
+++ testeGit/src/main/java/Main/TesteGit.java
@@ -11,6 +11,6 @@ package Main;
 public class TesteGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!222");
     }
 }
