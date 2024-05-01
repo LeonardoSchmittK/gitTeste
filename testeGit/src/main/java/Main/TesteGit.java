b8e60cdf31aa75e0e7b2080dfa5dd2cc4e637fdb
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-01 00:52:39 -0300

    aaaa

--- testeGit/src/main/java/Main/TesteGit.java
+++ testeGit/src/main/java/Main/TesteGit.java
@@ -11,6 +11,6 @@ package Main;
 public class TesteGit {
 
     public static void main(String[] args) {
-        System.out.println("Vamos!");
+        System.out.println("Vamos, CHARLIIEEEEE!");
     }
 }
